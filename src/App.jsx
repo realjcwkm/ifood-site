2f491d66851379cf42eb776fb03628f91e621ce0
realjcwkm <realjcwkm@gmail.com>
2022-07-03 14:53:54 -0400

    creating react project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Header from './Header';
+import Galeria from './Galeria.jsx';
+
+function App(){
+    return(
+        <>
+            <Header/>
+            <Galeria />
+        </>
+    );
+}
+
+export default App;
